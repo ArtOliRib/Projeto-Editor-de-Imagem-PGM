dc87135f9bd870e82b0d2116c881cddd97dfe572
Gustavo Paixao <luizgustavopaixa1@gmail.com>
2022-12-03 15:31:08 -0300

    Adicionando a versão do projeto com a função que cria o arquivo clarear e alguns arquivos de imagem

--- projeto.c
+++ projeto.c
@@ -9,41 +9,71 @@ typedef struct{
     int matriz [512][512];
 } pgm;
 
-int main(){
-    setlocale(LC_ALL,"portuguese");
+//------------------------------------------------//
+//Essa função serve apenas para testes
+//preencher uma matriz 512/512 para testes
+void fill(int m[][512])
+{
+    for (int i = 0; i < 512; i++)
+        for (int j = 0; j < 512; j++)
+            m[i][j] = rand() % 100;
+    return;
+}
 
-    FILE *img1 = fopen("lal.txt","a");
-
+//-------------------------------------------------//
+//Função para criar o arquivo mais claro
+void clarear() {
+    FILE *IMG1 = fopen("CLAREAR.pgm","a");
     pgm tam;
     int i,j;
-    tam.linha = 1;
-    tam.coluna = 1;
-    tam.maior = 50;
-    tam.matriz[0][0] = 1;
 
-    /*for ( i=0; i<10;i++)
-            for ( j=0; j<10;j++){
-                scanf ("%i", tam.matriz[ i ][ j ]);
-    }*/
+    //tamanho dado por mim, precisa ser alterado para ler o valor do arquivo
+    tam.linha = 512;
+    tam.coluna = 512;
+    tam.maior = 50;
+
+    //preencher matriz para testes
+    fill (tam.matriz);
+
     //Cria o cabeçalho da primeira imagem alterada
-    fprintf(img1, "P2");
-    fputc('\n', img1);
-    fprintf(img1, "%d %d", tam.linha, tam.coluna);
-    fputc('\n', img1);
-    fprintf(img1, "%d", tam.maior);
-    fputc('\n', img1);
+    fprintf(IMG1, "P2");
+    fputc('\n', IMG1);
+    fprintf(IMG1, "%d %d", tam.linha, tam.coluna);
+    fputc('\n', IMG1);
+    fprintf(IMG1, "%d", tam.maior);
+    fputc('\n', IMG1);
+
+    //Coloca a matriz no arquivo criado
     for(i = 0; i < tam.linha;i++){
         for(j = 0; j < tam.coluna;j++){
-            fprintf(img1,"%i",tam.matriz[i][j]);
+            fprintf(IMG1,"%i ",tam.matriz[i][j]);
         }
     }
+    fclose (IMG1);
+};
 
+//-------------------------------------------------//
 
+int main(int argc, char *argv[]){
+    setlocale(LC_ALL,"portuguese");
 
-    while(getchar() != '\n')
+    //abre o arquivo principal pelo CMD
+    FILE *img = fopen(argv[1], "r");
 
-            ;
-        getchar();
-        return 0;
+        //verifica se o arquivo foi aberto corretamente
+        if (img == NULL)
+            printf("Falha ao abrir imagem");
+        else
+            printf("Imagem pgm aberta");
+
+    //fecha o arquivo principal
+    fclose(img);
+
+    //chama a função clarear e cria o primeiro arquivo
+    clarear();
+
+    while(getchar() != '\n');
+    getchar();
+    return 0;
 
 }
