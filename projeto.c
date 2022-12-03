ed14bd63c954fdf3bff9618156e5e95ccdb02846
Gustavo Paixao <luizgustavopaixa1@gmail.com>
2022-12-03 18:42:53 -0300

    Adicionado o fscanf para pegar os valores de linha, coluna, maior e atribuir na struct

--- projeto.c
+++ projeto.c
@@ -9,53 +9,15 @@ typedef struct{
     int matriz [512][512];
 } pgm;
 
-//------------------------------------------------//
-//Essa função serve apenas para testes
-//preencher uma matriz 512/512 para testes
-void fill(int m[][512])
-{
-    for (int i = 0; i < 512; i++)
-        for (int j = 0; j < 512; j++)
-            m[i][j] = rand() % 100;
-    return;
-}
-
-//-------------------------------------------------//
-//Função para criar o arquivo mais claro
-void clarear() {
-    FILE *IMG1 = fopen("CLAREAR.pgm","a");
-    pgm tam;
-    int i,j;
-
-    //tamanho dado por mim, precisa ser alterado para ler o valor do arquivo
-    tam.linha = 512;
-    tam.coluna = 512;
-    tam.maior = 50;
-
-    //preencher matriz para testes
-    fill (tam.matriz);
-
-    //Cria o cabeçalho da primeira imagem alterada
-    fprintf(IMG1, "P2");
-    fputc('\n', IMG1);
-    fprintf(IMG1, "%d %d", tam.linha, tam.coluna);
-    fputc('\n', IMG1);
-    fprintf(IMG1, "%d", tam.maior);
-    fputc('\n', IMG1);
-
-    //Coloca a matriz no arquivo criado
-    for(i = 0; i < tam.linha;i++){
-        for(j = 0; j < tam.coluna;j++){
-            fprintf(IMG1,"%i ",tam.matriz[i][j]);
-        }
-    }
-    fclose (IMG1);
-};
-
 //-------------------------------------------------//
 
 int main(int argc, char *argv[]){
     setlocale(LC_ALL,"portuguese");
+
+    char tipo [3];
+    int linha, coluna, maior;
+
+    pgm m;
 
     //abre o arquivo principal pelo CMD
     FILE *img = fopen(argv[1], "r");
@@ -63,17 +25,62 @@ int main(int argc, char *argv[]){
         //verifica se o arquivo foi aberto corretamente
         if (img == NULL)
             printf("Falha ao abrir imagem");
-        else
-            printf("Imagem pgm aberta");
+            
+        else {
+            printf("Imagem pgm aberta\n");
+            fscanf(img, "%s %d %d %d", &tipo, &linha, &coluna, &maior);
+            printf("%s\n%d %d\n%d", tipo, linha, coluna, maior);
+            printf("\n");}
 
-    //fecha o arquivo principal
+
     fclose(img);
 
+    m.linha = linha;
+    m.coluna = coluna;
+    m.maior = maior;
+
     //chama a função clarear e cria o primeiro arquivo
-    clarear();
+    //FUNÇÃO NÃO ESTÁ R
+    clarear(linha, coluna, maior);
 
     while(getchar() != '\n');
     getchar();
     return 0;
 
 }
+
+
+//-------------------------------------------------//
+//Função para criar o arquivo mais claro
+//ESSA FUNÇÃO NÃO ESTÁ RODANDO
+void clarear(int linha, int coluna, int maior) 
+    {
+    printf("INICIO DA FUNÇÃO,  %d", maior);
+    
+    pgm m;
+    int i,j;
+    FILE *IMG1 = fopen ("CLAREAR.pgm","a");
+
+    //preencher matriz para testes
+
+    //Cria o cabeçalho da primeira imagem alterada
+    fprintf(IMG1, "P2");
+    fputc('\n', IMG1);
+    fprintf(IMG1, "%d %d", linha, coluna);
+    fputc('\n', IMG1);
+    fprintf(IMG1, "%d", maior);
+    fputc('\n', IMG1);
+
+    //Coloca a matriz no arquivo criado
+    for(i = 0; i < m.linha; i++){
+        for(j = 0; j < m.coluna;j++){
+            fprintf(IMG1,"%i ", m.matriz[i][j]);
+        }
+    }
+    fclose (IMG1);
+};
+
+
+//------------------------------------------------//
+//Essa função serve apenas para testes
+//preencher uma matriz 512/512 para testes
